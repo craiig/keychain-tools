10c730461d4916206203b6d0450646b4dbeddf99
craig <craiig@gmail.com>
2017-12-07 14:35:45 -0800

    working scripts and makefile that cleared all querie

--- tpcds_analysis/postgres_explain.py
+++ tpcds_analysis/postgres_explain.py
@@ -6,9 +6,12 @@ from pprint import pprint
 import sys
 import json
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Explain a TPC-DS SQL Query as Json')
 parser.add_argument('filename', help='filename of sql query')
+parser.add_argument('--output', '-o', help='output file for json')
+parser.add_argument('--query', '-q', help='print out the query (useful for debugging)', action='store_true')
 args = parser.parse_args()
 
 with open(args.filename) as fh:
@@ -27,8 +30,17 @@ EXPLAIN (format json, verbose)
 {query}
 """.format(schema=tpcds_schema, query=to_explain)
 
+if(args.query):
+    print query
+    sys.exit(0)
+
 cur.execute(query)
 r = cur.fetchone()
-print json.dumps(r['QUERY PLAN'], indent=1)
+
+if(args.output != None):
+    with open(args.output, "w+") as fh:
+        json.dump(r['QUERY PLAN'], fh, indent=1)
+else:
+    print json.dumps(r['QUERY PLAN'], indent=1)
 
 cur.close()
