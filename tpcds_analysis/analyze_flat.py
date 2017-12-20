b833c44aa1650c2019c2fe5094e55cde0889dfa0
craig <craiig@gmail.com>
2017-12-20 13:48:49 -0800

    pandas dataframes as a stopgap to making me upload this to a real sql service

--- tpcds_analysis/analyze_flat.py
+++ tpcds_analysis/analyze_flat.py
@@ -5,7 +5,7 @@ import json
 import sqlparse
 import sys
 from pprint import pprint
-from collections import Counter
+import pandas as pd
 import re
 
 parser = argparse.ArgumentParser(description='analyze flattened plans')
@@ -29,6 +29,15 @@ id_map = {}
 def parser_preprocess(sql):
     # needed because sql parse does not recognize bpchar as a built-in
     sql = re.sub("::bpchar", "::text", sql) # synonynms for the same type
+
+    #canonicalize subplan references
+    #what is the difference between SubPlan \d and $2?
+    # SubPLan is a plan that has dependencies on other parts of the query
+    # InitPlan ($\d)+ references are plans that have no dependencies and can be executed once
+    #  InitPlans are as good as constants assuming no updates to the underlying data happens
+    #  for our purposes we group them together as one
+    sql = re.sub("SubPlan \d+", "subplan_reference", sql)
+    sql = re.sub("\$\d+", "subplan_reference", sql)
     return sql
 
 def parse(sql):
@@ -53,7 +62,6 @@ def remove_outer_paranthesis(node):
 
 def erase_identifier_names_recur(node, in_function=False, debug=False):
     # walk each parse node and modify any identifier name into basic name
-    #TODO identifier map
     global identifier_numbering, id_map
     #only replace identifiers when we are not in a function and it matches the standard column name regex
     if debug:
@@ -61,7 +69,10 @@ def erase_identifier_names_recur(node, in_function=False, debug=False):
         if hasattr(node,'tokens'):
             print "\ttokens: {}".format(node.tokens)
     if type(node) == sqlparse.sql.Identifier and not in_function and re.match("^[0-9A-z_\.]*$", node.value):
-        if node.value not in id_map:
+        if re.match('^subplan_reference$', node.value):
+            # skip
+            name = node.value
+        elif node.value not in id_map:
             #TODO we are erasing too much, what about constants????
             # maybe this is ok??
             # only replace standard column names
@@ -73,6 +84,15 @@ def erase_identifier_names_recur(node, in_function=False, debug=False):
         if debug:
             print 'old: {} new: {}'.format(node.value, name)
         node.tokens = [sqlparse.sql.Token(sqlparse.tokens.Name, name)]
+    elif type(node) == sqlparse.sql.Identifier and in_function and re.match("^[0-9A-z_\.]*$", node.value):
+        #replace function names
+        func_renames = {
+                'avg': 'agg'
+                ,'sum': 'agg'
+        }
+        if node.value in func_renames:
+            node.tokens = [sqlparse.sql.Token(sqlparse.tokens.Name, func_renames[node.value])]
+
     elif type(node) == sqlparse.sql.Token and (node.ttype == sqlparse.tokens.Number.Integer or node.ttype == sqlparse.tokens.Number.Float):
         if debug:
             print 'number: {}'.format(node)
@@ -97,10 +117,10 @@ def transform_statement(ty, stmnt):
     o = str(erase_identifier_names(parse(o)))
 
     r = {
-        "col" : "[0-9A-z_\.]+"
-        , "op": "(<|>|==|<=|>=|=)"
+        #"col" : "[0-9A-z_\.]+"
+        "col" : "identifier[0-9]+"
+        , "op": "(<|>|==|<=|>=|=|/|-|\*)"
         , "non_commute_op": "(\|\|)"
-        #, "binary_op": "{col} ({op}) {col}"
         , "text_constant": "'[^']+?'::text"
         #, "function": "[A-z]+\({col}.*?\)"
     }
@@ -111,7 +131,12 @@ def transform_statement(ty, stmnt):
 
     #o = re.sub("^'.*'::text$", "'.*'::text constant", o) 
     o = re.sub("{text_constant}".format(**r), "text_constant", o) #pretty common
-    o = re.sub("{text_constant}".format(**r), "text_constant", o) #pretty common
+
+    #high level ops
+    o = re.sub("{col} ({op}) {col}".format(**r), "col op col", o)
+    o = re.sub("{col} ({op}) \({col}\)::numeric".format(**r), "col op col", o)
+    o = re.sub("\({col}\)::numeric ({op}) {col}".format(**r), "col op col", o)
+    o = re.sub("{col} ({op}) (numeric|text)_constant".format(**r), "col op constant", o)
 
     o = re.sub("^\(([^\(\)]*?)\)$", "\\1", o) #eliminate outer parenthesis as long as there are no inner paranthesis
     #o = re.sub("^sum\([0-9A-z_\.]*\)$", "sum(column)", o) #explicitly match sum
@@ -129,39 +154,48 @@ def transform_statement(ty, stmnt):
     #o = re.sub("{col} ({op}) {function}".format(**r), "col1 \\1 col2", o)
     return o
 
-outputs = Counter()
-all_output_cols = 0;
+#want to satisfy a few queries:
+# total occurences of an expression
+# number of unique queries expression appears in
+#
+dfQuery = pd.DataFrame(columns=['query','type','expression'])
 def analyze_outputs(p):
-    global outputs
-    global all_output_cols
+    global dfQuery
 
-    def process_statement(ty, in_stmnt):
-        global outputs
-        global all_output_cols
+    def process_statement(query, ty, in_stmnt):
+        global dfQuery
         # TODO ignore some nodes to avoid 'over counting'
         # because basic col outputs are very high, (or we just don't report it in that way)
         #transform outputs
+        # the way we'll avoid this is giving occurences relative to different queries
+        # instead of relative to some 'total number of expressions'
         out_stmnt = transform_statement(ty,in_stmnt)
 
         if args.filter and args.filter == out_stmnt:
             print in_stmnt
  
-        outputs[out_stmnt] += 1
-        all_output_cols = all_output_cols + 1
+        obj = {
+            'query': query,
+            'type': ty,
+            'expression': out_stmnt
+        }
+        dfQuery = dfQuery.append(obj, ignore_index=True);
+        #outputs[out_stmnt] += 1
+        #all_output_cols = all_output_cols + 1
 
     #what are the column transformations?
     if "Output" in p:
         for o in p['Output']:
-            process_statement('output',o)
+            process_statement(p['filename'], 'output',o)
             pass
 
     if "Filter" in p:
         #print p['Filter']
-        process_statement('filter',p['Filter'])
+        process_statement(p['filename'], 'filter',p['Filter'])
         pass
 
     for k in p.get("Group Key", []):
-        process_statement('group', k)
+        process_statement(p['filename'], 'group', k)
         pass
 
     def flatten_group_sets(gsets):
@@ -173,12 +207,15 @@ def analyze_outputs(p):
     if("Grouping Sets" in p):
         groups = [item for item in flatten_group_sets(p['Grouping Sets'])]
         for g in groups:
-            process_statement('group', g)
+            process_statement(p['filename'], 'group', g)
 
 def outputs_stats():
-    print "total output columns: %s" % (all_output_cols)
-    print "total unique cols after post-proessing: {}".format(len(outputs))
-    pprint( outputs.most_common(len(outputs)) )
+    global dfQuery
+    #print "total output columns: %s" % (all_output_cols)
+    #print "total unique cols after post-proessing: {}".format(len(outputs))
+    #pprint( outputs.most_common(len(outputs)) )
+    print dfQuery
+    pass
 
 # testing the sql parse identifier erasure
 #test = "(catalog_sales.cs_ext_sales_price - COALESCE(catalog_returns.cr_return_amount, 0.0))"
