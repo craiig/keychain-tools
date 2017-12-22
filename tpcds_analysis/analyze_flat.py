c766307ef5d7d4b338454cf65c0341cf78bc24e3
craig <craiig@gmail.com>
2017-12-21 16:01:22 -0800

    in progress dealing with casts more gracefully

--- tpcds_analysis/analyze_flat.py
+++ tpcds_analysis/analyze_flat.py
@@ -12,6 +12,8 @@ parser = argparse.ArgumentParser(description='analyze flattened plans')
 parser.add_argument('--filename', '-f', help='filename of plans')
 parser.add_argument('--test', '-t', help='test statement to parse')
 parser.add_argument('--filter', help='print all original statements that match the given string, used in conjunction with --filename')
+parser.add_argument('--filter_re', help='print all original statements that match the given regex, used in conjunction with --filename')
+parser.add_argument('--print_sorted', '-s', help='sort by expression before outputting', action='store_true')
 args = parser.parse_args()
 
 # The overall methodology here is to group statements together that DO NOT have
@@ -32,18 +34,42 @@ def parser_preprocess(sql):
     # needed because sql parse does not recognize bpchar as a built-in
     sql = re.sub("::bpchar", "::text", sql) # synonynms for the same type
 
-    #canonicalize subplan references
-    #what is the difference between SubPlan \d and $2?
+    # SUBPLAN references:
+    # we treat subplan references as basically columns because they are the
+    # results of prior computations run over the data, or computations running
+    # concurrently (once per row), in which case they are just treated as
+    # columns on the current data set
+
+    # 1. a subplan quirk from Postgres is that there may be optional plan
+    # alternatives that are selected at runtime, so we fold them up here
+    # http://grokbase.com/t/postgresql/pgsql-general/12bwsdmsg7/alternatives
+    sql = re.sub("\(alternatives: SubPlan (\d+) or hashed SubPlan \d\)", "subplan.ref\\1", sql)
+
+    #2. what is the difference between SubPlan \d and $2?
     # SubPlan is a plan that has dependencies on other parts of the query
     # InitPlan ($\d)+ references are plans that have no dependencies and can be executed once
     #  InitPlans are as good as constants assuming no updates to the underlying data happens
     #  for our purposes we group them together as one
-    sql = re.sub("SubPlan \d+", "subplan_reference", sql)
-    sql = re.sub("\$\d+", "subplan_reference", sql)
+    sql = re.sub("SubPlan (\d+)", "subplan.ref\\1", sql)
+    sql = re.sub("\$(\d+)", "subplan.ref\\1", sql)
+
+    # 3. another subplan reference fix to some subquery scans that
+    # have interesting alias names, when they just seem to be normal subqueries
+    sql = re.sub("\"\*SELECT\* \d+\".", "alias.", sql)
+
+    #simplify numeric casts
+    sql = re.sub("::numeric\(\d+,\d+\)", "::numeric", sql) 
+
+    if args.test:
+        print "sql post process: {}".format(sql)
+
     return sql
 
 def parse(sql):
-    return sqlparse.parse(parser_preprocess(sql))[0]
+    p = sqlparse.parse(parser_preprocess(sql))[0]
+    if args.test:
+        print "parse: {}".format(p)
+    return p
 
 def erase_identifier_names(node, debug=False):
     global identifier_numbering, id_map
@@ -55,9 +81,22 @@ def erase_identifier_names(node, debug=False):
 def remove_outer_paranthesis(node):
     #remove any outer paranthesis so we can keep it similar
     if type(node) == sqlparse.sql.Statement and len(node.tokens) == 1  and type(node.tokens[0]) == sqlparse.sql.Parenthesis:
-        #print 'remove outer: {}'.format(type(node.tokens[0]) == sqlparse.sql.Parenthesis) 
         parans = node.tokens[0]
-        node.tokens = [parans.tokens[1]]
+        while len(parans.tokens) == 3 and type(parans.tokens[1]) == sqlparse.sql.Parenthesis:
+            #unroll any further nesting too
+            if args.test:
+                print "unroll another parans, tokens: {}".format(parans.tokens)
+            parans = parans.tokens[1]
+
+        #node.tokens = [parans.tokens[1]]
+        # remove the first and last token nodes
+        # test case: "((date_dim.d_date >= '1999-02-01') and (date_dim.d_date <= '1999-04-02 00:00:00'))"
+        node.tokens = parans.tokens[1:-1]
+        if args.test:
+            print "remove outer paranthesis: {} tokens: {} parans tokens: {}".format(node, node.tokens, parans.tokens)
+
+    if args.test:
+        print "post remove outer paranthesis: {}".format(node)
 
     return node
     pass
@@ -65,12 +104,19 @@ def remove_outer_paranthesis(node):
 def erase_identifier_names_recur(node, in_function=False, debug=False):
     # walk each parse node and modify any identifier name into basic name
     global identifier_numbering, id_map
+
+    #set in function now, because we may unset it later when we do a coalesce replacement
+    next_in_function = type(node)==sqlparse.sql.Function
+
     #only replace identifiers when we are not in a function and it matches the standard column name regex
     if debug:
-        print "---> node: {} type: {} tokenlist: {}".format(node, type(node), isinstance(node, sqlparse.sql.TokenList))
+        print "---> node: {} type: {} tokenlist: {} in_function: {}".format(
+                node, type(node), isinstance(node, sqlparse.sql.TokenList), in_function)
         if hasattr(node,'tokens'):
             print "\ttokens: {}".format(node.tokens)
     if type(node) == sqlparse.sql.Identifier and not in_function and re.match("^[0-9A-z_\.]*$", node.value):
+        if debug:
+            print "identifier replacement"
         if re.match('^subplan_reference$', node.value):
             # skip
             name = node.value
@@ -88,9 +134,15 @@ def erase_identifier_names_recur(node, in_function=False, debug=False):
         node.tokens = [sqlparse.sql.Token(sqlparse.tokens.Name, name)]
     elif type(node) == sqlparse.sql.Identifier and in_function and re.match("^[0-9A-z_\.]*$", node.value):
         #replace function names
+        if debug:
+            print "agg replacement"
         func_renames = {
                 'avg': 'agg'
                 ,'sum': 'agg'
+                ,'max': 'agg'
+                ,'min': 'agg'
+                ,'stddev_samp': 'agg'
+                ,'round': 'func'
         }
         if node.value in func_renames:
             node.tokens = [sqlparse.sql.Token(sqlparse.tokens.Name, func_renames[node.value])]
@@ -106,19 +158,41 @@ def erase_identifier_names_recur(node, in_function=False, debug=False):
             cand = cand.tokens[0]
         if debug:
             print "coalesce replacement candidate: {} type: {} tokens: {}".format( cand, type(cand), cand.tokens)
+        if(cand.tokens[0].value == '(' and cand.tokens[-1].value == ')'):
+            if debug:
+                print "trimming parenthesis from coalesce in: {} out: {}".format(cand.tokens, cand.tokens[1:-1])
+            cand.tokens = cand.tokens[1:-1]
         node.tokens = [cand]
+        #TODO coalesce needs to trip column identifier stuff too
+        #since we removed coalesce we can mark ourseles as not being in a function anymore
+        next_in_function = False
         pass
     elif type(node) == sqlparse.sql.Token and (node.ttype == sqlparse.tokens.Number.Integer or node.ttype == sqlparse.tokens.Number.Float):
+        #detect numeric constants
         if debug:
             print 'number: {}'.format(node)
         node.value = 'numeric_constant'
+    elif type(node) == sqlparse.sql.Parenthesis:
+        #fold parenthesis
+        while (len(node.tokens) == 3 and 
+            type(node.tokens[0]) == sqlparse.sql.Token and node.tokens[0].value == '('
+            and type(node.tokens[1]) == sqlparse.sql.Parenthesis
+            and type(node.tokens[2]) == sqlparse.sql.Token and node.tokens[2].value == ')'
+        ):
+            #unroll any further nesting too
+            candidate = node.tokens[1]
+            node.tokens = node.tokens[1].tokens
+            #print "token[0]: {} val: {} token[1]: {} token[2]: {}".format(type(node.tokens[0]), node.tokens[0].value,  type(node.tokens[1]), type(node.tokens[2]))
+            if args.test:
+                print "remove nested parenthesis, candidate: {} candidate.tokens: {} node.tokens: {}".format(candidate, candidate.tokens, node.tokens)
 
+    #recurse 
     if isinstance(node, sqlparse.sql.TokenList):
         for n in node:
             #to avoid re-writing parts of identifiers, including any casts, we won't descend into identifiers
             # this is actually preventing a re-write of an expression
             #if type(node) != sqlparse.sql.Identifier:
-            erase_identifier_names_recur(n, in_function=type(node)==sqlparse.sql.Function, debug=debug)
+            erase_identifier_names_recur(n, in_function=next_in_function, debug=debug)
     if debug:
         print "<---"
 
@@ -134,24 +208,45 @@ def transform_statement(ty, stmnt):
     r = {
         #"col" : "[0-9A-z_\.]+"
         "col" : "identifier[0-9]+"
-        , "op": "(<|>|==|<=|>=|=|/|-|\*)"
+        , "op": "(<|>|==|<=|>=|=|/|-|\*|<>|\+)"
         , "non_commute_op": "(\|\|)"
         , "text_constant": "'[^']+?'::text"
+        , "date_constant": "'[^']+?'::date"
         #, "function": "[A-z]+\({col}.*?\)"
     }
     # one level of substitution lets us reference other patterns more easily
     for k in r.iterkeys():
         r[k] = r[k].format(**r)
 
-
     #o = re.sub("^'.*'::text$", "'.*'::text constant", o) 
     o = re.sub("{text_constant}".format(**r), "text_constant", o) #pretty common
+    o = re.sub("'[^']+?'::numeric".format(**r), "numeric_constant", o) 
+    o = re.sub("'[^']+?'::integer".format(**r), "numeric_constant", o) 
+    o = re.sub("NULL::numeric".format(**r), "numeric_constant", o) 
+    o = re.sub("NULL::integer".format(**r), "numeric_constant", o) 
+    o = re.sub("NULL::bigint".format(**r), "numeric_constant", o) 
+    o = re.sub("'[^']+?'::timestamp without time zone", "time_constant", o)
+    o = re.sub("{date_constant}".format(**r), "time_constant", o) 
+
+    #fix numeric casts on identifiers and expressions since we don't really care
+    #note some of this is in the regex preprocessor too
+    o = re.sub("\(({col})\)::numeric".format(**r), "\\1", o)
+    #o = re.sub("\(({col})\)text".format(**r), "\\1", o)
+
+    #TODO stop replacing here to perform a summary of:
+    # # of cols, # of identifiers, # of operators used for each query
 
     #high level ops
     o = re.sub("{col} ({op}) {col}".format(**r), "col op col", o)
-    o = re.sub("{col} ({op}) \({col}\)::numeric".format(**r), "col op col", o)
-    o = re.sub("\({col}\)::numeric ({op}) {col}".format(**r), "col op col", o)
-    o = re.sub("{col} ({op}) (numeric|text)_constant".format(**r), "col op constant", o)
+    #o = re.sub("{col} ({op}) \({col}\)::numeric".format(**r), "col op col", o) # are these two needed?
+    #o = re.sub("\({col}\)::numeric ({op}) {col}".format(**r), "col op col", o)
+    o = re.sub("{col} ({op}) (numeric|text|time)_constant".format(**r), "col op constant", o)
+
+    #some control flow patterns
+    # i think a few conditional aggregations would be good to add to a test
+    o = re.sub("^agg\(CASE WHEN \(.*\) THEN numeric_constant ELSE numeric_constant END\)$", "conditional aggregate", o)
+    o = re.sub("^agg\(CASE WHEN \(.*\) THEN {col} ELSE numeric_constant END\)$".format(**r), "conditional aggregate", o)
+    o = re.sub("agg\(CASE WHEN \(.*\) THEN .*? ELSE .*? END\)".format(**r), "conditional aggregate", o) #catch all to isolate this category entirely
 
     o = re.sub("^\(([^\(\)]*?)\)$", "\\1", o) #eliminate outer parenthesis as long as there are no inner paranthesis
     #o = re.sub("^sum\([0-9A-z_\.]*\)$", "sum(column)", o) #explicitly match sum
@@ -188,9 +283,11 @@ def analyze_outputs(p):
             out_stmnt = transform_statement(ty,in_stmnt)
         except Exception as inst:
             print "exception parsing statement: {}".format(in_stmnt)
-            print inst
+            raise
 
         if args.filter and args.filter == out_stmnt:
+            print in_stmnt
+        if args.filter_re and re.match(args.filter_re, out_stmnt):
             print in_stmnt
  
         obj = {
@@ -228,15 +325,22 @@ def analyze_outputs(p):
 
 def outputs_stats():
     global dfQuery
+    global unique_expressions_per_query
     dfQuery = pd.DataFrame(data=queries, columns=['query','type','expression'])
     #print dfQuery
 
-    unique_expressions_per_query = dfQuery.groupby(['expression'])['query'].nunique().sort_values()
+    unique_expressions_per_query = dfQuery.groupby(['expression'])['query'].nunique().sort_values().to_frame()
+    unique_expressions_per_query.reset_index(level=0, inplace=True)
 
     print "Total expressions: {}".format( len(unique_expressions_per_query) )
+
     with pd.option_context('display.max_rows', None):
         # expressions PER query!! cool
-        print unique_expressions_per_query.to_string()
+        if args.print_sorted:
+            pprint( unique_expressions_per_query.sort_values('expression').to_dict('records') )
+        else:
+            #print unique_expressions_per_query.to_string()
+            pprint( unique_expressions_per_query.to_dict('records') )
 
 if(args.test):
     r = erase_identifier_names(parse(args.test), debug=True)
@@ -250,5 +354,5 @@ with open(args.filename) as fh:
 for p in plans:
     analyze_outputs(p)
 
-if not args.filter:
+if not args.filter and not args.filter_re:
     outputs_stats()
