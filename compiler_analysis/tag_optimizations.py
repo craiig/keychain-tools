4d517b80090a3e8bc8b7f1a720bb32f7b913339c
craig <craiig@gmail.com>
2018-02-12 12:32:50 -0800

    cleaned up and reordered optimization db, now to review and add llvm

--- compiler_analysis/tag_optimizations.py
+++ compiler_analysis/tag_optimizations.py
@@ -7,14 +7,17 @@ import os
 from bs4 import BeautifulSoup
 import sys
 import tempfile
+import collections
 
 def load_database(filename):
-    database = {}
+    #database = {}
+    database = collections.OrderedDict()
     if filename and os.path.exists(filename):
         print "loading existing program json"
         try:
             with open(filename) as fh:
-                database = json.load(fh)
+                #https://stackoverflow.com/questions/6921699/can-i-get-json-to-load-into-an-ordereddict-in-python
+                database = collections.OrderedDict(json.load(fh, object_pairs_hook=collections.OrderedDict))
         except ValueError as e:
             print "error parsing json"
             print e
@@ -30,14 +33,19 @@ def parse_gcc(db, filename):
     with open(filename) as fh:
         soup = BeautifulSoup(fh, "lxml")
 
+    # for GCC we only include optimizations included in the default
+    # optimizations ignoring experimental and elective floating point opts
+    # if we wish to include these later, change what gets added to opt_list
+
     lists = soup.select('dl')
     opt_list = [c for c in lists[1].children]
 
     all_opts = []
     current_opt = None
     seen_desc = False
-    while len(opt_list) > 0:
-        c = opt_list.pop(0)
+    #while len(opt_list) > 0:
+    for c in opt_list:
+        #c = opt_list.pop(0)
         if c.name == 'dt':
             if current_opt:
                 if not seen_desc:
@@ -59,16 +67,34 @@ def parse_gcc(db, filename):
 
             current_opt['description'] = c.text.encode('utf-8')
 
+    #insert last element
+    if current_opt and seen_desc:
+        all_opts.append( current_opt )
+
     #apply to db
     # TODO tag these optimizations as coming from GCC
     for o in all_opts:
         if not o['name'] in db:
             db[o['name']] = {}
+        #else:
+            ##ordered dict hack, do a removal and reinsert to establish good order (to fix a badly ordered file)
+            ## can remove after this is done once?
+            #entry = db[o['name']]
+            #del db[o['name']]
+            #db[o['name']] = entry
 
         entry = db[o['name']]
         entry['description'] = o.get('description', None)
         if 'alternate_names' in o:
             entry['alternate_names'] = o['alternate_names']
+        entry['origin'] = 'gcc'
+
+    return all_opts
+
+def remove_old_entries(db, new_entries):
+    for k,v in db.iteritems():
+        if k not in new_entries:
+            print "delete {}".format(k)
 
 def manually_tag_opts(db, args):
     # show a vim window with the opt description and a window to add tags
@@ -86,7 +112,7 @@ def manually_tag_opts(db, args):
 
         desc_fh = tempfile.NamedTemporaryFile(mode="w+", delete=True)
         desc_file = desc_fh.name
-        if 'description' in o:
+        if 'description' in o and o['description'] != None:
             desc_fh.write(name)
             desc_fh.write("\n")
             if 'alternate_names' in o:
@@ -125,12 +151,19 @@ if __name__ == "__main__":
     parser.add_argument('--gcc_parse', '-g', help='parse gcc html docs and enrich database')
     parser.add_argument('--manually_tag', '-m', help='manually tag the optimizatins with tags', action='store_true')
     parser.add_argument('--manually_skip_tagged', '-mst', help='skip already tagged', action='store_true')
+
+    parser.add_argument('--remove_old_entries', help="remove old entries", action='store_true')
+
     args = parser.parse_args()
 
     db = load_database(args.db)
 
     if args.gcc_parse:
-        parse_gcc(db, args.gcc_parse)
+        gcc_entries = parse_gcc(db, args.gcc_parse)
+
+        if args.remove_old_entries:
+            new_entries = [e['name'] for e in gcc_entries]
+            remove_old_entries(db, new_entries)
 
     if args.manually_tag:
         manually_tag_opts(db, args)
